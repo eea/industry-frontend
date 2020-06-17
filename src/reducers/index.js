6d0d42da8df97fc098642564b99af1072aada327
Miu Razvan <miu.razvan28@gmail.com>
2020-06-17 20:08:42 +0300

    Load '?fullboject' into data for DetailedLink

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,20 @@
 import defaultReducers from '@plone/volto/reducers';
+<<<<<<< Updated upstream
 
 const reducers = {
     ...defaultReducers
 }
+=======
+import section_tabs from './section_tabs';
+import parent_folder_data from './parent_folder_data';
+import pages from './pages';
 
-export default reducers
+const reducers = {
+  section_tabs,
+  parent_folder_data,
+  pages,
+  ...defaultReducers,
+};
+>>>>>>> Stashed changes
+
+export default reducers;
