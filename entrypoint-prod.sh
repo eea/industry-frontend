3f0afe389aff9dbbe45e813d796d17f999414e38
razvanMiu <miu.razvan28@gmail.com>
2021-02-22 17:00:01 +0200

    Clean up entrypoint

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -36,28 +36,6 @@ if [ -z "$CYPRESS_API_PATH" ]; then
   CYPRESS_API_PATH="$RAZZLE_API_PATH"
 fi
 
-if [ -z "$GIT_URL" ]; then
-  GIT_URL="https://github.com"
-fi
-
-if [ -z "$GIT_BRANCH" ]; then
-  GIT_BRANCH="master"
-fi
-
-if [ -z "$GIT_USER" ]; then
-  GIT_USER="eea"
-fi
-
-if [ -z "$GIT_NAME" ]; then
-  echo "GIT_NAME is required"
-  exit 1
-fi
-
-PACKAGE="$GIT_NAME"
-if [ ! -z "$NAMESPACE" ]; then
-  PACKAGE="$NAMESPACE/$GIT_NAME"
-fi
-
 if [[ "$1" == "cypress"* ]]; then
   RAZZLE_API_PATH=$RAZZLE_API_PATH yarn start &
 
